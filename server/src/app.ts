330f4568de34c3b93e2bbfe159cf983ebdc01a3e
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-07-08 03:10:40 +0600

    fixed cors issues of frontend - 9.0

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,39 +1,44 @@
 import express, { Application, Request, Response } from "express";
-import cors from "cors"
-import bookController, { BookCreateController, BookEditController } from "./app/controllers/book_controllers";
+import cors from "cors";
+import bookController, {
+  BookCreateController,
+  BookEditController,
+} from "./app/controllers/book_controllers";
 import globalErrorHandler from "./app/middleware/globalError";
 import borrowRouter from "./app/controllers/borrow_controllers";
 
 const app: Application = express();
 
 const allowedOrigins = [
-  "http://localhost:5173", 
-  "https://your-frontend-name.vercel.app" 
+  "http://localhost:5173",
+  "https://librarymy.vercel.app",
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-app.use("/books", bookController)
-app.use("/create-book", BookCreateController)
-app.use("/edit-book", BookEditController)
-app.use("/borrow", borrowRouter)
-
+app.use("/books", bookController);
+app.use("/create-book", BookCreateController);
+app.use("/edit-book", BookEditController);
+app.use("/borrow", borrowRouter);
 
 app.get("/", (req: Request, res: Response) => {
-  res.redirect("/books")
+  res.redirect("/books");
 });
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 export default app;
