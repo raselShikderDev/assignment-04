2acd663d31907bbf6efb15ac6832024d8d3f3709
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-07-08 01:19:02 +0600

    fixed cors issues 7.0

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,10 +6,23 @@ import borrowRouter from "./app/controllers/borrow_controllers";
 
 const app: Application = express();
 
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "https://your-frontend-name.vercel.app" 
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true
 }));
+
 app.use(express.json());
 app.use("/books", bookController)
 app.use("/create-book", BookCreateController)
